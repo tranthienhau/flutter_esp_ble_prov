a2e5d9a4c9dc1f4cb4c0b804b9c39efdd66d86cb
Hau Tran <tranthienhau@gmail.com>
2026-01-28 15:04:50 +0700

    feat: Add mac address field to device class.

--- lib/src/device.dart
+++ lib/src/device.dart
@@ -1,31 +1,38 @@
 class Device {
   final String name;
   final String deviceId;
+  final String macAddress;
 
   Device({
     required this.name,
     required this.deviceId,
+    required this.macAddress,
   });
 
   factory Device.fromNativeString(String nativeString) {
     final parts = nativeString.split('_');
     if (parts.length >= 3) {
       final name = '${parts[0]}_${parts[1]}';
+      final macAddress = parts[1];
       final deviceId = parts.sublist(2).join('_');
-      return Device(name: name, deviceId: deviceId);
+      return Device(name: name, deviceId: deviceId, macAddress: macAddress);
     }
-    return Device(name: nativeString, deviceId: '');
+    return Device(name: nativeString, deviceId: '', macAddress: '');
   }
 
   @override
-  String toString() => 'Device(name: $name, deviceId: $deviceId)';
+  String toString() =>
+      'Device(name: $name, deviceId: $deviceId, macAddress: $macAddress)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    return other is Device && other.name == name && other.deviceId == deviceId;
+    return other is Device &&
+        other.name == name &&
+        other.deviceId == deviceId &&
+        other.macAddress == macAddress;
   }
 
   @override
-  int get hashCode => name.hashCode ^ deviceId.hashCode;
+  int get hashCode => name.hashCode ^ deviceId.hashCode ^ macAddress.hashCode;
 }
